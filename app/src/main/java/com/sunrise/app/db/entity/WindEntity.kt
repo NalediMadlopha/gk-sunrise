a543804c0d0b94e198b26ca11541d02e61d2f5cf
Naledi Madlopha <naledi.madlopha@gmail.com>
2021-04-13 01:29:35 +0200

    chore: Implement Database Entities
    * Implement the database entities used to contain the data that
    will be stored in the database

---
+++ app/src/main/java/com/sunrise/app/db/entity/WindEntity.kt
@@ -0,0 +1,23 @@
+package com.sunrise.app.db.entity
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.Ignore
+import com.sunrise.app.domain.model.Wind
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "Wind")
+data class WindEntity(
+    @ColumnInfo(name = "deg")
+    val deg: Double?,
+    @ColumnInfo(name = "speed")
+    val speed: Double?
+) : Parcelable {
+    @Ignore
+    constructor(wind: Wind?) : this(
+        deg = wind?.deg,
+        speed = wind?.speed
+    )
+}
