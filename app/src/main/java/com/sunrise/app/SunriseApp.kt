bede50e31c3f770d1235cba035a7f9644dd3909d
Naledi Madlopha <naledi.madlopha@gmail.com>
2021-04-12 21:29:20 +0200

    feat: Setup Dependency Injection
    * Implement Dagger for dependency injection

---
+++ app/src/main/java/com/sunrise/app/SunriseApp.kt
@@ -0,0 +1,22 @@
+package com.sunrise.app
+
+import android.app.Application
+import com.sunrise.app.di.AppInjector
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import javax.inject.Inject
+
+class SunriseApp : Application(), HasAndroidInjector {
+
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector() = androidInjector
+
+    override fun onCreate() {
+        super.onCreate()
+
+        AppInjector.init(this)
+    }
+}
---
+++ app/src/main/java/com/sunrise/app/di/component/AppComponent.kt
@@ -0,0 +1,24 @@
+package com.sunrise.app.di.component
+
+import android.app.Application
+import com.sunrise.app.SunriseApp
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AndroidSupportInjectionModule::class])
+interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): AppComponent
+    }
+
+    fun inject(sunriseApp: SunriseApp)
+
+}
