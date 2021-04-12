a34559bdbeefd57d0aee1256f837c689a4f27eed
Naledi Madlopha <naledi.madlopha@gmail.com>
2021-04-13 01:31:08 +0200

    chore: Implement Database
    * Implement the Room database to be used to store
    data on the device

---
+++ app/src/main/java/com/sunrise/app/utils/DataConverter.kt
@@ -0,0 +1,55 @@
+package com.sunrise.app.utils
+
+import androidx.room.TypeConverter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import com.sunrise.app.domain.model.ListItem
+import com.sunrise.app.domain.model.WeatherItem
+
+object DataConverter {
+
+    @TypeConverter
+    @JvmStatic
+    fun stringToList(data: String?): List<ListItem>? {
+        if (data == null) {
+            return emptyList()
+        }
+
+        val moshi = Moshi.Builder().build()
+        val type = Types.newParameterizedType(List::class.java, ListItem::class.java)
+        val adapter = moshi.adapter<List<ListItem>>(type)
+        return adapter.fromJson(data)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun listToString(objects: List<ListItem>): String {
+        val moshi = Moshi.Builder().build()
+        val type = Types.newParameterizedType(List::class.java, ListItem::class.java)
+        val adapter = moshi.adapter<List<ListItem>>(type)
+        return adapter.toJson(objects)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun weatherStringToList(data: String?): List<WeatherItem>? {
+        if (data == null) {
+            return emptyList()
+        }
+
+        val moshi = Moshi.Builder().build()
+        val type = Types.newParameterizedType(List::class.java, WeatherItem::class.java)
+        val adapter = moshi.adapter<List<WeatherItem>>(type)
+        return adapter.fromJson(data)
+    }
+
+    @TypeConverter
+    @JvmStatic
+    fun weatherListToString(objects: List<WeatherItem>): String {
+        val moshi = Moshi.Builder().build()
+        val type = Types.newParameterizedType(List::class.java, WeatherItem::class.java)
+        val adapter = moshi.adapter<List<WeatherItem>>(type)
+        return adapter.toJson(objects)
+    }
+
+}
