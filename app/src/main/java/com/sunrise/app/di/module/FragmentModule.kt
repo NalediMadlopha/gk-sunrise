fbf02b3411e111a6a851c18e65f5a209a513cc09
Naledi Madlopha <naledi.madlopha@gmail.com>
2021-04-13 20:44:40 +0200

    feat: Implement Dagger Module

---
+++ app/src/main/java/com/sunrise/app/di/module/FragmentModule.kt
@@ -0,0 +1,12 @@
+package com.sunrise.app.di.module
+
+import dagger.Module
+
+
+@Module
+abstract class FragmentBuildersModule {
+
+//    @ContributesAndroidInjector
+//    abstract fun contributeMainFragment(): MainFragment
+
+}
---
+++ app/src/main/java/com/sunrise/app/di/module/NetworkModule.kt
@@ -0,0 +1,72 @@
+package com.sunrise.app.di.module
+
+import android.os.Environment
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.sunrise.app.core.Constants
+import com.sunrise.app.domain.DefaultRequestInterceptor
+import com.sunrise.app.domain.datasource.OpenWeatherMapApi
+import dagger.Module
+import dagger.Provides
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    @Named("cached")
+    fun provideOkHttpClient(): OkHttpClient {
+        val cache = Cache(Environment.getDownloadCacheDirectory(), 10 * 1024 * 1024)
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .addInterceptor(DefaultRequestInterceptor())
+            .readTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .cache(cache)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    @Named("non_cached")
+    fun provideNonCachedOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .addInterceptor(DefaultRequestInterceptor())
+            .readTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi, @Named("cached") client: OkHttpClient): Retrofit.Builder {
+        return Retrofit.Builder()
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+    }
+
+    @Provides
+    @Singleton
+    fun provideService(retrofit: Retrofit.Builder): OpenWeatherMapApi {
+        return retrofit.baseUrl(Constants.Service.BASE_URL)
+            .build()
+            .create(OpenWeatherMapApi::class.java)
+    }
+}
